fd8d047148862a089a87a8b988268954fa56b6dc
Izel Ozhan <izelozhan@gmail.com>
2025-04-23 14:18:35 -0500

    create methods

---
+++ SearchInventory/src/main/java/com/pluralsight/Product.java
@@ -0,0 +1,39 @@
+package com.pluralsight;
+
+public class Product {
+    int productId;
+    String name;
+    double price;
+
+
+    public Product (int productId, String name, double price) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
