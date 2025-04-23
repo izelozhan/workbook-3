830fd468df0fb98dbb23333084e7ada9ff6fdf43
Izel Ozhan <izelozhan@gmail.com>
2025-04-23 14:10:20 -0500

    workbook-3 2-2 exercise-3

--- Mod03/SearchEngineLogger/src/main/java/SearchEngineLogger.java
+++ Mod03/SearchEngineLogger/src/main/java/SearchEngineLogger.java
@@ -49,22 +49,3 @@ public class SearchEngineLogger {
         }
     }
 }
-//
-//Create a Java application named SearchEngineLogger.
-//Create a method to log the actions of the user. Write each user action to a file
-//name logs.txt.
-//Actions include:
-//a. Launching the application
-//b. Searching for a term
-//c. Exiting the application
-//In the main() method prompt the user for a search term that they wish to
-//search.
-//        Example
-//2-16
-//Enter a search term (X to exit):
-//Example
-//Entries in the logs.txt file should follow this format:
-//        2023-09-06 12:42:20 launch
-// search : How to use ChatGPT
-//2023-09-06 12:43:51 search : How to forge a camp knife
-//2023-09-06 12:45:32 exit
