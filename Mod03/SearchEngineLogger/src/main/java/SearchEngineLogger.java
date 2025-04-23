bca5042fad145953b1ac10fa80447ea72e7f8cfb
Izel Ozhan <izelozhan@gmail.com>
2025-04-23 14:08:38 -0500

    workbook-3 mod03 exercise-2

--- Mod03/SearchEngineLogger/src/main/java/SearchEngineLogger.java
+++ Mod03/SearchEngineLogger/src/main/java/SearchEngineLogger.java
@@ -1,5 +1,70 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class SearchEngineLogger {
     public static void main(String[] args) {
+        LocalDateTime today = LocalDateTime.now();
+//        2023-09-06 12:42:45
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDate = today.format(fmt);
 
+        try {
+            Scanner scanner = new Scanner(System.in);
+            FileWriter writer = new FileWriter("logs.txt");
+            boolean exit = false;
+
+            while (exit == false) {
+                System.out.println("Enter a search term (X to exit) \n");
+                System.out.println("a. Launching the application");
+                System.out.println("b. Searching for a term");
+                System.out.println("c. Exiting the application");
+                String input = scanner.nextLine();
+
+                switch (input.trim()) {
+                    case "a":
+                        writer.write(formattedDate + " launch" + "\n");
+                        System.out.println(formattedDate + " launch");
+                        break;
+                    case "b":
+                        System.out.println("What do you want to search?");
+                        String searchInput = scanner.nextLine();
+
+                        writer.write(formattedDate + " search : " + searchInput + "\n");
+                        System.out.println(formattedDate + " search : " + searchInput);
+                        break;
+                    case "c", "x":
+                        writer.write(formattedDate + " exit" + "\n");
+                        System.out.println(" exit");
+                        exit = true;
+                        break;
+
+                }
+            }
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+//
+//Create a Java application named SearchEngineLogger.
+//Create a method to log the actions of the user. Write each user action to a file
+//name logs.txt.
+//Actions include:
+//a. Launching the application
+//b. Searching for a term
+//c. Exiting the application
+//In the main() method prompt the user for a search term that they wish to
+//search.
+//        Example
+//2-16
+//Enter a search term (X to exit):
+//Example
+//Entries in the logs.txt file should follow this format:
+//        2023-09-06 12:42:20 launch
+// search : How to use ChatGPT
+//2023-09-06 12:43:51 search : How to forge a camp knife
+//2023-09-06 12:45:32 exit
